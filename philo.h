bde2eadb246a0dba58295d8338e08c8bc3c37fa0
Kilian Pouliquen <kipouliq@paul-f5Ar3s5.clusters.42paris.fr>
2024-03-26 18:22:06 +0100

    working on passing data to philos at creation

--- philo.h
+++ philo.h
@@ -6,19 +6,34 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 15:55:25 by kipouliq          #+#    #+#             */
-/*   Updated: 2024/03/22 16:02:22 by kipouliq         ###   ########.fr       */
+/*   Updated: 2024/03/26 17:59:35 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+# include <pthread.h>
+# include <stdio.h>
+# include <sys/time.h>
+# include <stdlib.h>
+
+typedef struct timeval	t_timeval;
+
+typedef struct time
+{
+	long int			time_sec;
+	float				time_ms;
+}						t_time;
 
 typedef struct philo
 {
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int nb_of_meals;
-}   philo;
+    pthread_mutex_t     *forks;
+    int                 philo_nb;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+    int                 time_to_think;
+	int					nb_of_meals;
+}						t_philo;
 
 #endif
--- philosophers.c
+++ philosophers.c
@@ -6,16 +6,25 @@
 /*   By: kipouliq <kipouliq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 12:21:14 by kipouliq          #+#    #+#             */
-/*   Updated: 2024/03/22 17:07:15 by kipouliq         ###   ########.fr       */
+/*   Updated: 2024/03/26 18:21:04 by kipouliq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/time.h>
-#include <stdio.h>
-#include <pthread.h>
 #include "./philo.h"
 
 // ./philo 4 100 100 100 5
+
+t_time	get_time_elapsed(t_timeval *starting_time)
+{
+	t_timeval	current_time;
+	t_time		time_elapsed;
+
+	gettimeofday(&current_time, NULL);
+	time_elapsed.time_sec = current_time.tv_sec - starting_time->tv_sec;
+	time_elapsed.time_ms = ((float)current_time.tv_usec
+			- (float)starting_time->tv_usec) / 1000;
+	return (time_elapsed);
+}
 
 int	ft_atoi(const char *nptr)
 {
@@ -44,22 +53,119 @@ int	ft_atoi(const char *nptr)
 	return (nb);
 }
 
-int create_philos(pthread_t *philos)
+int	check_forks(t_philo *data)
 {
-    
 }
 
-int main(int argc, char **argv)
+void	philo_routine(t_philo *data)
 {
-    pthread_t   *philos_ids;
-    int         philos_nb;
-    
-    if (argc != 5)
-        return ;
-    philos_nb = ft_atoi(argv[1]);
-    philos_ids = malloc(sizeof(pthread_t) * (philos_nb + 1));
-    if (!philos_ids)
-        return (-1);
-    philos_ids[philos_nb] = NULL;
-    create_philos(philos_ids);
+	int	meals_finished;
+
+	meals_finished = 0;
+	while (meals_finished < data->nb_of_meals)
+	{
+	}
+}
+
+pthread_t	*create_philos(int philos_nb, t_philo **all_data)
+{
+	pthread_t	*philos_tab;
+	t_philo		*data;
+	int			i;
+	int			error;
+
+	philos_tab = malloc(sizeof(pthread_t) * (philos_nb));
+	if (!philos_tab)
+		return (NULL);
+	i = -1;
+	while (++i < philos_nb)
+	{
+		error = pthread_create(&philos_tab[i], NULL, (void *)philo_routine,
+				data);
+		if (error)
+		{
+			printf("error creating pthread\n");
+			return (NULL);
+		}
+	}
+	return (philos_tab);
+}
+
+pthread_mutex_t	*create_forks(int forks_nb)
+{
+	pthread_mutex_t	*forks;
+	int				i;
+	int				error;
+
+	forks = malloc(sizeof(pthread_mutex_t) * (forks_nb));
+	if (!forks)
+		return (NULL);
+	i = -1;
+	while (++i < forks_nb)
+	{
+		error = pthread_mutex_init(&forks[i], NULL);
+		if (error)
+		{
+			printf("mutex error : %d\n", error);
+			return (NULL);
+		}
+	}
+	return (forks);
+}
+
+int	wait_philos(pthread_t *philos_tab, int philos_nb)
+{
+	int	i;
+	int	error;
+
+	i = -1;
+	while (++i < philos_nb)
+	{
+		error = pthread_join(philos_tab[i], NULL);
+		if (error)
+			return (error);
+	}
+	return (0);
+}
+
+t_philo 	**create_philo_data_structs(int philos_nb, char **argv,
+		pthread_mutex_t *forks_tab)
+{
+	t_philo **data_structs;
+    t_philo *philo_data;
+    int	i;
+
+    data_structs = malloc(sizeof(t_philo *) * (ft_atoi(argv[0])));
+    if (!data_structs)
+        return (NULL);
+	data->forks = forks_tab;
+	data->time_to_die = ft_atoi(argv[1]);
+	data->time_to_eat = ft_atoi(argv[2]);
+	data->time_to_sleep = ft_atoi(argv[3]);
+	data->nb_of_meals = ft_atoi(argv[4]);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_philo			**philos_data;
+	pthread_t		*philos;
+	pthread_mutex_t	*forks;
+	int				philos_nb;
+
+	if (argc != 6)
+		return (-1);
+	argv += 1;
+	philos_nb = ft_atoi(argv[0]);
+	forks = create_forks(philos_nb);
+	if (!forks)
+		return (-1);
+	philos_data = create_philo_data_structs(argv, forks);
+	philos = create_philos(philos_nb, philos_data);
+	if (!philos)
+	{
+		return (-1);
+	}
+	printf("pthread_join = %d\n", wait_philos(philos, philos_nb));
+	return (0);
 }
